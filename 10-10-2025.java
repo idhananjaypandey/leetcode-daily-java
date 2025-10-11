0ad0d8110dda504360f5bc08568e24dd66ee9f6c
Dhananjay <dhananjay.pandey72@gmail.com>
2025-10-11 10:41:42 +0530

    10-10-2025

---
+++ 10-10-2025.java
@@ -0,0 +1,10 @@
+// 3147. Taking Maximum Energy From the Mystic Dungeon
+
+class Solution {
+  public int maximumEnergy(int[] energy, int k) {
+    int[] dp = energy.clone();
+    for (int i = energy.length - 1 - k; i >= 0; --i)
+      dp[i] += dp[i + k];
+    return Arrays.stream(dp).max().getAsInt();
+  }
+}
