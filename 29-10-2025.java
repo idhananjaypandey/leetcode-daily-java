eec5fe3472b3b2451bafcf50194c3f48883d7954
Dhananjay <dhananjay.pandey72@gmail.com>
2025-10-29 17:13:13 +0530

    29-10-2025 3370. Smallest Number With All Set Bits

---
+++ 29-10-2025.java
@@ -0,0 +1,11 @@
+// 3370. Smallest Number With All Set Bits
+
+class Solution {
+  public int smallestNumber(int n) {
+    return (1 << bitLength(n)) - 1;
+  }
+
+  private int bitLength(int n) {
+    return Integer.SIZE - Integer.numberOfLeadingZeros(n);
+  }
+}
