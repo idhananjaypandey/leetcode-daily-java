215f7a36b830be53cc815d422fa795862199a747
Dhananjay <dhananjay.pandey72@gmail.com>
2025-11-14 18:45:44 +0530

    14-11-2025. 2536. Increment Submatrices by One

---
+++ november-2025/14-11-2025.java
@@ -0,0 +1,52 @@
+// 2536. Increment Submatrices by One
+
+class Solution {
+    public int[][] rangeAddQueries(int n, int[][] queries) {
+        int[][] matrix = new int[n][n];
+    
+        for (int[] query : queries) {
+            int row1 = query[0];
+            int col1 = query[1];
+            int row2 = query[2];
+            int col2 = query[3];
+        
+            matrix[row1][col1]++;
+        
+            if (row2 + 1 < n) {
+                matrix[row2 + 1][col1]--;
+            }
+          
+            // Mark the position to the right of the bottom-right corner with -1
+            if (col2 + 1 < n) {
+                matrix[row1][col2 + 1]--;
+            }
+          
+            // Mark the diagonal position (row2+1, col2+1) with +1 to compensate
+            if (row2 + 1 < n && col2 + 1 < n) {
+                matrix[row2 + 1][col2 + 1]++;
+            }
+        }
+      
+        // Build the final matrix using 2D prefix sum
+        for (int row = 0; row < n; row++) {
+            for (int col = 0; col < n; col++) {
+                // Add value from the cell above
+                if (row > 0) {
+                    matrix[row][col] += matrix[row - 1][col];
+                }
+              
+                // Add value from the cell to the left
+                if (col > 0) {
+                    matrix[row][col] += matrix[row][col - 1];
+                }
+              
+                // Subtract the diagonal cell (included twice) to avoid double counting
+                if (row > 0 && col > 0) {
+                    matrix[row][col] -= matrix[row - 1][col - 1];
+                }
+            }
+        }
+      
+        return matrix;
+    }
+}
