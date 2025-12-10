3bb9bac6395e09884f3f0abe2daba26029670e76
Dhananjay <dhananjay.pandey72@gmail.com>
2025-12-10 22:41:23 +0530

    3577. Count the Number of Computer Unlocking Permutations

---
+++ december-2025/10-12-2025.java
@@ -0,0 +1,18 @@
+// 3577. Count the Number of Computer Unlocking Permutations
+
+class Solution {
+    public int countPermutations(int[] complexity) {
+
+        final int MOD = 1_000_000_007;
+        long result = 1;
+      
+        for (int i = 1; i < complexity.length; i++) {
+    
+            if (complexity[i] <= complexity[0]) {
+                return 0;
+            }
+            result = (result * i) % MOD;
+        }
+        return (int) result;
+    }
+}
