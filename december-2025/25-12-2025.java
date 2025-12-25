1552da84921e597510ea9d4f631bf42218bdb174
Dhananjay <dhananjay.pandey72@gmail.com>
2025-12-25 11:12:20 +0530

    3075. Maximize Happiness of Selected Children

---
+++ december-2025/25-12-2025.java
@@ -0,0 +1,18 @@
+// 3075. Maximize Happiness of Selected Children
+
+class Solution {
+  public long maximumHappinessSum(int[] happiness, int k) {
+    final int n = happiness.length;
+    long ans = 0;
+    int decremented = 0;
+
+    Arrays.sort(happiness);
+
+    for (int i = n - 1; i >= n - k; --i) {
+      ans += Math.max(0, happiness[i] - decremented);
+      ++decremented;
+    }
+
+    return ans;
+  }
+}
