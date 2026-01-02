77f55414803faeffb9e75de5dc3bf0a42754817e
Dhananjay <dhananjay.pandey72@gmail.com>
2026-01-02 23:11:14 +0530

    961. N-Repeated Element in Size 2N Array

---
+++ january-2026/02-01-2026.java
@@ -0,0 +1,10 @@
+// 961. N-Repeated Element in Size 2N Array
+
+class Solution {
+  public int repeatedNTimes(int[] nums) {
+    for (int i = 0; i + 2 < nums.length; ++i)
+      if (nums[i] == nums[i + 1] || nums[i] == nums[i + 2])
+        return nums[i];
+    return nums[nums.length - 1];
+  }
+}
