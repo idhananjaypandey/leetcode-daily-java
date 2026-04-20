b72a0f2752fcf71402d8ed8f013e2c309a1c9dfb
Dhananjay <dhananjay.pandey72@gmail.com>
2026-04-20 18:01:48 +0530

    2078. Two Furthest Houses With Different Colors

---
+++ april-2026/20-04-2026.java
@@ -0,0 +1,14 @@
+// 2078. Two Furthest Houses With Different Colors
+
+class Solution {
+  public int maxDistance(int[] colors) {
+    final int n = colors.length;
+    int i = 0;     // the leftmost index, where colors[i] != colors[n - 1]
+    int j = n - 1; // the rightmost index, where colors[j] != colors[0]
+    while (colors[i] == colors[n - 1])
+      ++i;
+    while (colors[j] == colors[0])
+      --j;
+    return Math.max(n - 1 - i, j);
+  }
+}
