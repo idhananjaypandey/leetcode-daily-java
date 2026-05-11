a7d57cf8847977d3e060dad5f52b337b1f73bb09
Dhananjay <dhananjay.pandey72@gmail.com>
2026-05-11 23:10:23 +0530

    2553. Separate the Digits in an Array

---
+++ may-2026/11-05-2026.java
@@ -0,0 +1,13 @@
+// 2553. Separate the Digits in an Array
+
+class Solution {
+  public int[] separateDigits(int[] nums) {
+    List<Integer> ans = new ArrayList<>();
+
+    for (final int num : nums)
+      for (final char c : String.valueOf(num).toCharArray())
+        ans.add(c - '0');
+
+    return ans.stream().mapToInt(Integer::intValue).toArray();
+  }
+}
