ac68d5f0cd6581957d0511d20ee862a9da41601a
Dhananjay <dhananjay.pandey72@gmail.com>
2026-06-28 12:48:05 +0530

    1846. Maximum Element After Decreasing and Rearranging

---
+++ june-2026/28-06-2026.java
@@ -0,0 +1,13 @@
+// 1846. Maximum Element After Decreasing and Rearranging
+
+class Solution {
+  public int maximumElementAfterDecrementingAndRearranging(int[] arr) {
+    Arrays.sort(arr);
+    arr[0] = 1;
+
+    for (int i = 1; i < arr.length; ++i)
+      arr[i] = Math.min(arr[i], arr[i - 1] + 1);
+
+    return arr[arr.length - 1];
+  }
+}
