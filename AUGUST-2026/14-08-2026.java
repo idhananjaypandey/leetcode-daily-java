5a4ae7b794fe7850543994bf38b68a70ceec2d68
Dhananjay <dhananjay.pandey72@gmail.com>
2026-08-14 13:25:52 +0530

    3090. Maximum Length Substring With Two Occurrences

---
+++ AUGUST-2026/14-08-2026.java
@@ -0,0 +1,17 @@
+// 3090. Maximum Length Substring With Two Occurrences
+
+class Solution {
+  public int maximumLengthSubstring(String s) {
+    int ans = 0;
+    int[] count = new int[26];
+
+    for (int l = 0, r = 0; r < s.length(); ++r) {
+      ++count[s.charAt(r) - 'a'];
+      while (count[s.charAt(r) - 'a'] > 2)
+        --count[s.charAt(l++) - 'a'];
+      ans = Math.max(ans, r - l + 1);
+    }
+
+    return ans;
+  }
+}
