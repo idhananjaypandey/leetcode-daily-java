51b04765507df7c8d4562912915499483e533161
Dhananjay <dhananjay.pandey72@gmail.com>
2026-08-22 18:41:00 +0530

    3622. Check Divisibility by Digit Sum and Product

---
+++ AUGUST-2026/22-08-2026.java
@@ -0,0 +1,15 @@
+// 3622. Check Divisibility by Digit Sum and Product
+
+class Solution {
+    public boolean checkDivisibility(int n) {
+        int s = 0, p = 1;
+        int x = n;
+        while (x != 0) {
+            int v = x % 10;
+            x /= 10;
+            s += v;
+            p *= v;
+        }
+        return n % (s + p) == 0;
+    }
+}
