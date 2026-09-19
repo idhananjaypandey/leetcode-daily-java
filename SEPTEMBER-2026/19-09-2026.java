cfbff9e632eddc881d897494a234999cbedc94ac
Dhananjay <dhananjay.pandey72@gmail.com>
2026-09-19 21:04:03 +0530

    1401. Circle and Rectangle Overlapping

---
+++ SEPTEMBER-2026/19-09-2026.java
@@ -0,0 +1,13 @@
+// 1401. Circle and Rectangle Overlapping
+
+class Solution {
+    public boolean checkOverlap(int radius, int xCenter, int yCenter, int x1, int y1, int x2, int y2) {
+        int nearestX = Math.max(x1, Math.min(xCenter, x2));
+        int nearestY = Math.max(y1, Math.min(yCenter, y2));
+        
+        int deltaX = xCenter - nearestX;
+        int deltaY = yCenter - nearestY;
+        
+        return (deltaX * deltaX + deltaY * deltaY) <= (radius * radius);
+    }
+}
