4fe1d898d6b6aefd3268dc8a06a719a74a24d03b
Dhananjay <dhananjay.pandey72@gmail.com>
2026-09-20 06:11:18 +0530

    3498. Reverse Degree of a String

---
+++ SEPTEMBER-2026/20-09-2026.java
@@ -0,0 +1,12 @@
+// 3498. Reverse Degree of a String
+
+class Solution {
+  public int reverseDegree(String s) {
+    int ans = 0;
+    for (int i = 0; i < s.length(); ++i) {
+      final int reversePos = 26 - (s.charAt(i) - 'a');
+      ans += reversePos * (i + 1);
+    }
+    return ans;
+  }
+}
