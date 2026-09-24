4cbc63fc6312a84b536eaf9562cd736d84c8c38a
Dhananjay <dhananjay.pandey72@gmail.com>
2026-09-24 05:45:23 +0530

    3550. Smallest Index With Digit Sum Equal to Index

---
+++ SEPTEMBER-2026/24-09-2026.java
@@ -0,0 +1,19 @@
+// 3550. Smallest Index With Digit Sum Equal to Index
+
+class Solution {
+  public int smallestIndex(int[] nums) {
+    for (int i = 0; i < nums.length; ++i)
+      if (getDigitSum(nums[i]) == i)
+        return i;
+    return -1;
+  }
+
+  private int getDigitSum(int num) {
+    int sum = 0;
+    while (num > 0) {
+      sum += num % 10;
+      num /= 10;
+    }
+    return sum;
+  }
+}
